5c98d4c0bfed2431d319396d51748394f5005718
kawthar2002 <k31082002@gmail.com>
2023-02-22 00:06:35 +0300

    slider

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const swiper = new Swiper(
-  '.swiper',
-  {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-  }
-);
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
+const slider = new Swiper('.slider-inner__container', {
+  slidesPerView: 4,
+  navigation: {
+    nextEl: '.right',
+    prevEl: '.left',
+  },
+});
